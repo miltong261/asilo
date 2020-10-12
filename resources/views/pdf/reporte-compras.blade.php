17824e8e80494aa537832e79ae921e362bffa42b
Angel <ajaneljuarez23@gmail.com>
2020-10-12 12:56:13 -0600

    reportes corregidos

--- resources/views/pdf/reporte-compras.blade.php
+++ resources/views/pdf/reporte-compras.blade.php
@@ -3,26 +3,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reporte de Compras</title>
-    <link rel="stylesheet" href="/var/www/html/proyectos/asilo/public/assets/css/style-compras.css">
-    <link rel="stylesheet" href="/var/www/html/proyectos/asilo/public/assets/css/main.min.css">
-    <script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
+    <link rel="stylesheet" href="C:/xampp/htdocs/asilo/resources/assets/css/style-compras.css">
+    <link rel="stylesheet" href="../../assets/css/style-compras.css">  
+     <script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
     <body>
         <header>
             @foreach ($encabezado_compra as $encabezado)
             <table class="text-center" id="tabla-encabezado">
                 <tr>
-                    <td id="td-img"> <img src="/var/www/html/proyectos/asilo/public/assets/img/logo-pdf.jpeg" alt="Logo asilo" id="imagen"> </td>
-                    <td>  <h3 class="text-secondary"> ASILO DE ANCIANOS RETALHULEU</h3>
+                    <td id="td-img">
+                        <img src="C:/xampp/htdocs/asilo/resources/assets/img/logo-pdf.jpeg" alt="Logo asilo" id="imagen">
+                    </td>
+                    <td> 
+                        <h4 > ASILO DE ANCIANOS RETALHULEU</h4>
                         <h5> Residenciales Ciudad Palmeras </h5>
                         <h5>Cantón Recuerdo Ocosito, Retalhuleu </h5>
                         <h4> BODEGA DE INSUMOS</h4>
-                    <td> <h3 class="text-secondary" id="codigo">{{ $encabezado_compra }}</h3> </td>
+                    <td style="display: table-cell;vertical-align: middle;">
+                        <h4 id="codigo">{{ $encabezado->codigo }}</h4>
+                    </td>
                 </tr>
             </table>
 
             <table id="tabla-empleado">
                 <tr>
-                    <td style="width: 20%" > <strong>Documento: </strong></td>
+                    <td style="width: 30%" > <strong>Documento: </strong></td>
                     <td > {{ $encabezado->documento }} </td>
                 </tr>
                 <tr>
@@ -40,44 +45,36 @@
         @foreach ($detalle_compra as $index => $detalle)
         <!-- INICIO TABLA -->
         <div class="table-responsive">
-                <table id="tabla-datos" class="text-center table ">
-                    <thead>
-                        <tr id="fila-encabezado">
-                            <th style="width: 10%;">  <i class="fas fa-hashtag"></i> </th>
-                            <th> <i class="fas fa-qrcode"></i> CÓDIGO</th>
-                            <th> <i class="fas fa-store"></i> NOMBRE</th>
-                            <th> <i class="fas fa-store"></i> PRESENTACIÓN</th>
-                            <th> <i class="fas fa-search"></i> OBSERVACIÓN</th>
-                            <th> <i class="fas fa-sort-numeric-up-alt"></i> Cantidad</th>
-                            <th>Precio</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody >
-                        <tr>
-                            <td>1</td>
-                            <td >Shaun Park</td>
-                            <td>10/08/2019</td>
-                            <td>320</td>
-                            <td > Observación</td>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>100</td>
-                        </tr>
-
-                        <tr>
-                            <td>1</td>
-                            <td >Shaun Park</td>
-                            <td>10/08/2019</td>
-                            <td>320</td>
-                            <td > Observación</td>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>100</td>
-                        </tr>
-
-                    </tbody>
-                </table>
+            <table id="tabla-datos" class="text-center table ">
+                <thead>
+                    <tr id="fila-encabezado">
+                        <th style="width: 10%;"> # </th>
+                        <th> <i class="fas fa-qrcode"></i> CÓDIGO</th>
+                        <th> <i class="fas fa-store"></i> NOMBRE</th>
+                        <th> PRECIO </th>
+                        <th> <i class="fas fa-store"></i> PRESENTACIÓN</th>
+                        <th> <i class="fas fa-search"></i> OBSERVACIÓN</th>
+                        <th> <i class="fas fa-sort-numeric-up-alt"></i> CANTIDAD</th>
+                    </tr>
+                </thead>
+                <tbody  style="text-align: center;">
+                    <tr>
+                        <td>{{ $index+1 }}</td>
+                        <td>{{ $detalle->codigo_producto }}</td>
+                        <td >{{ $detalle->nombre_producto }}</td>
+                        <td> 1 </td>
+                        <td>{{ $detalle->presentacion_producto }}</td>
+                        <td>{{ $detalle->observacion_producto }}</td>
+                        <td > {{ $detalle->cantidad }}</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th style="  color:#5c1ac3;" colspan="6">TOTAL</th>
+                        <th> 1</th>
+                    </tr>
+                </tfoot>
+            </table>
         </div> <!--fin tabla responsive -->
         <!-- FIN TABLA DATOS -->
 
@@ -86,18 +83,19 @@
                 <tr>
                     <td >Recibió (F)</td>
                     <td class="info"></td>
-
-                    <td>Entregó </td>
-                    <td class="info"></td>
-
-                    <td>Autorizó (F)</td>
+                    <td>Nombre </td>
                     <td class="info"></td>
                 </tr>
+
                 <tr>
+                    <td>Autorizó (F)</td>
+                    <td class="info"></td>
                     <td>Nombre</td>
                     <td class="info"></td>
+                </tr>
 
-                    <td>Nombre</td>
+                <tr>
+                    <td>Entregó (F)</td>
                     <td class="info"></td>
 
                     <td>Nombre</td>
--- resources/views/pdf/reporte-donaciones.blade.php
+++ resources/views/pdf/reporte-donaciones.blade.php
@@ -4,26 +4,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reporte de Donaciones</title>
-    <link rel="stylesheet" href="/var/www/html/proyectos/asilo/public/assets/css/style-donaciones.css">
-    <link rel="stylesheet" href="/var/www/html/proyectos/asilo/public/assets/css/main.min.css">
+    <link rel="stylesheet" href="C:/xampp/htdocs/asilo/resources/assets/css/style-donaciones.css">
+    <link rel="stylesheet" href="../../assets/css/style-donaciones.css">  
     <script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
     <body>
         <header>
             @foreach ($encabezado_donacion as $encabezado)
             <table class="text-center" id="tabla-encabezado">
                 <tr>
-                    <td id="td-img"> <img src="/var/www/html/proyectos/asilo/public/assets/img/logo-pdf.jpeg" alt="Logo asilo" id="imagen"> </td>
-                    <td>  <h3 class="text-secondary"> ASILO DE ANCIANOS RETALHULEU</h3>
+                    <td id="td-img">
+                        <img src="C:/xampp/htdocs/asilo/resources/assets/img/logo-pdf.jpeg" alt="Logo asilo" id="imagen">
+                    </td>
+                    <td> 
+                        <h4 > ASILO DE ANCIANOS RETALHULEU</h4>
                         <h5> Residenciales Ciudad Palmeras </h5>
                         <h5>Cantón Recuerdo Ocosito, Retalhuleu </h5>
                         <h4> BODEGA DE INSUMOS</h4>
-                    <td> <h3 class="text-secondary" id="codigo">{{ $encabezado->codigo }}</h3> </td>
+                    <td style="display: table-cell;vertical-align: middle;">
+                        <h4 id="codigo">{{ $encabezado->codigo }}</h4>
+                    </td>
                 </tr>
             </table>
 
             <table id="tabla-donador">
                 <tr>
-                    <td style="width: 20%" > <strong>Donador: </strong></td>
+                    <td style="width: 8%;" > <strong>Donador: </strong></td>
                     <td > {{ $encabezado->donador }} </td>
                 </tr>
                 <tr>
@@ -45,7 +50,7 @@
             <table id="tabla-datos" class="text-center table ">
                 <thead>
                     <tr id="fila-encabezado">
-                        <th style="width: 10%;">  <i class="fas fa-hashtag"></i> </th>
+                        <th style="width: 10%;">  # </th>
                         <th> <i class="fas fa-qrcode"></i> CÓDIGO</th>
                         <th> <i class="fas fa-store"></i> NOMBRE</th>
                         <th> <i class="fas fa-store"></i> PRESENTACIÓN</th>
@@ -53,15 +58,14 @@
                         <th> <i class="fas fa-sort-numeric-up-alt"></i> Cantidad</th>
                     </tr>
                 </thead>
-                <tbody class="text-center">
+                <tbody style="text-align: center;">
                     <tr>
                         <td>{{ $index+1 }}</td>
                         <td>{{ $detalle->codigo_producto }}</td>
-                        <td >{{ $detalle->nombre_producto }}</td>
+                        <td>{{ $detalle->nombre_producto }}</td>
                         <td>{{ $detalle->presentacion_producto }}</td>
                         <td>{{ $detalle->observacion_producto }}</td>
-                        <td class="text-center">{{ $detalle->cantidad }}</td>
-                        <td>1</td>
+                        <td>{{ $detalle->cantidad }}</td>
                     </tr>
                 </tbody>
             </table>

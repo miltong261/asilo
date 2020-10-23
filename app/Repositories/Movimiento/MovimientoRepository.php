ac1d01658790cc93257363b81aa5b8a8181ed5c9
Milton Girón <lopezgl261@gmail.com>
2020-10-23 15:33:44 -0600

    Se agrego para ver movimientos por mes

--- app/Repositories/Movimiento/MovimientoRepository.php
+++ app/Repositories/Movimiento/MovimientoRepository.php
@@ -110,4 +110,156 @@ class MovimientoRepository extends BaseRepository
             }
         }
     }
+
+    public function saldoMes($month)
+    {
+        $anio = date('Y');
+        $montoSalida = 0;
+        $montoEntrada = 0;
+        $caja = $this->saldoCaja();
+        $saldoInicial = 0;
+
+        if (date('L', strtotime("$anio-02-01"))) {
+            $months = [
+                'Enero' => [
+                    $anio . '-01-01',
+                    $anio . '-01-31'
+                ],
+                'Febrero' => [
+                    $anio . '-02-01',
+                    $anio . '-02-29'
+                ],
+                'Marzo' => [
+                    $anio . '-03-01',
+                    $anio . '-03-31'
+                ],
+                'Abril' => [
+                    $anio . '-04-01',
+                    $anio . '-04-30'
+                ],
+                'Mayo' => [
+                    $anio . '-05-01',
+                    $anio . '-05-31'
+                ],
+                'Junio' => [
+                    $anio . '-06-01',
+                    $anio . '-06-30'
+                ],
+                'Julio' => [
+                    $anio . '-07-01',
+                    $anio . '-07-31'
+                ],
+                'Agosto' => [
+                    $anio . '-08-01',
+                    $anio . '-08-31'
+                ],
+                'Septiembre' => [
+                    $anio . '-09-01',
+                    $anio . '-09-30'
+                ],
+                'Octubre' => [
+                    $anio . '-10-01',
+                    $anio . '-10-31'
+                ],
+                'Noviembre' => [
+                    $anio . '-11-01',
+                    $anio . '-11-30'
+                ],
+                'Diciembre' => [
+                    $anio . '-12-01',
+                    $anio . '-12-31'
+                ]
+            ];
+        } else {
+            $months = [
+                'Enero' => [
+                    $anio . '-01-01',
+                    $anio . '-01-31'
+                ],
+                'Febrero' => [
+                    $anio . '-02-01',
+                    $anio . '-02-28'
+                ],
+                'Marzo' => [
+                    $anio . '-03-01',
+                    $anio . '-03-31'
+                ],
+                'Abril' => [
+                    $anio . '-04-01',
+                    $anio . '-04-30'
+                ],
+                'Mayo' => [
+                    $anio . '-05-01',
+                    $anio . '-05-31'
+                ],
+                'Junio' => [
+                    $anio . '-06-01',
+                    $anio . '-06-30'
+                ],
+                'Julio' => [
+                    $anio . '-07-01',
+                    $anio . '-07-31'
+                ],
+                'Agosto' => [
+                    $anio . '-08-01',
+                    $anio . '-08-31'
+                ],
+                'Septiembre' => [
+                    $anio . '-09-01',
+                    $anio . '-09-30'
+                ],
+                'Octubre' => [
+                    $anio . '-10-01',
+                    $anio . '-10-31'
+                ],
+                'Noviembre' => [
+                    $anio . '-11-01',
+                    $anio . '-11-30'
+                ],
+                'Diciembre' => [
+                    $anio . '-12-01',
+                    $anio . '-12-31'
+                ]
+            ];
+        }
+
+        $data = ['inicio' => $months[$month][0], 'fin' => $months[$month][1]];
+
+        $movimientos = $this->getModel()
+        ->join('users', 'users.id', '=', 'movimientos.user_id')
+        ->join('tipo_movimiento', 'tipo_movimiento.id', '=', 'movimientos.tipo_movimiento_id')
+        ->select('movimientos.*',
+            'users.usuario as nombre_usuario',
+            'users.rol_id',
+            'tipo_movimiento.id as tipo_movimiento_id',
+            'tipo_movimiento.nombre as tipo_movimiento_nombre',
+            'tipo_movimiento.entrada',
+            'tipo_movimiento.salida'
+        )
+        ->orderBy('movimientos.no_transaccion', 'desc')
+        ->whereBetween('movimientos.fecha_registro', [$data['inicio'], $data['fin']])
+        ->whereYear('movimientos.fecha_registro', $anio)
+        ->get();
+
+        foreach ($movimientos as $movimiento) {
+            if ($movimiento->salida == 1)
+                $montoSalida += $movimiento->monto;
+        }
+
+        foreach ($movimientos as $movimiento) {
+            if ($movimiento->entrada == 1)
+                $montoEntrada += $movimiento->monto;
+        }
+
+        $saldoInicial = $caja[0]['saldo'] + $montoSalida - $montoEntrada ;
+
+        if ($movimientos == '') {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'No hay registro de notas para la fecha '
+            ]);
+        } else {
+            return ['movimientos' => $movimientos, 'salida' => $montoSalida, 'entrada' => $montoEntrada, 'saldo_inicial' => $saldoInicial, 'saldo_actual' => $caja[0]['saldo']];
+        }
+    }
 }
